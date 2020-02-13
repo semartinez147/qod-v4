e754e8dee2e4f88cbb701fbc18ddbaac912b9650
Nick Bennett <nick@nickbenn.com>
2020-02-13 16:35:39 -0700

    More PUT/DELETE fun.

--- src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
+++ src/main/java/edu/cnm/deepdive/qod/controller/QuoteController.java
@@ -7,13 +7,16 @@ import edu.cnm.deepdive.qod.service.SourceRepository;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,6 +48,32 @@ public class QuoteController {
     return quoteRepository.findById(id).get();
   }
 
+  @PutMapping(value = "{id}",
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Quote put(@PathVariable UUID id, @RequestBody Quote modifiedQuote) {
+    Quote quote = get(id);
+    quote.setText(modifiedQuote.getText());
+    return quoteRepository.save(quote);
+  }
+
+  @PutMapping(value = "{id}/text",
+      consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+  public String put(@PathVariable UUID id, @RequestBody String modifiedQuote) {
+    Quote quote = get(id);
+    quote.setText(modifiedQuote);
+    quoteRepository.save(quote);
+    return quote.getText();
+  }
+
+  @DeleteMapping(value = "{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable UUID id) {
+    // Code below throws NoSuchElementException if id is not in database.
+//    Quote quote = get(id);
+//    quoteRepository.delete(quote);
+    quoteRepository.findById(id).ifPresent(quoteRepository::delete);
+  }
+
   @PutMapping(value = "{quoteId}/sources/{sourceId}", produces = MediaType.APPLICATION_JSON_VALUE)
   public Quote attach(@PathVariable UUID quoteId, @PathVariable UUID sourceId) {
     Quote quote = get(quoteId);
@@ -64,5 +93,23 @@ public class QuoteController {
     return quote;
   }
 
+  @DeleteMapping(value = "{quoteId}/sources/{sourceId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void detach(@PathVariable UUID quoteId, @PathVariable UUID sourceId) {
+    Quote quote = get(quoteId);
+    Source source = sourceRepository.findById(sourceId).get();
+    List<Source> sources = quote.getSources();
+    boolean matchFound = false;
+    for (Source s : sources) {
+      if (s.getId().equals(source.getId())) {
+        matchFound = true;
+        break;
+      }
+    }
+    if (matchFound) {
+      sources.remove(source);
+      quoteRepository.save(quote);
+    }
+  }
 
 }
